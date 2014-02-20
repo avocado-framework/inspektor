f2a7080f33fc7d49fdf38dce0c544e2083d34db7
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-02-20 11:23:17 -0300

    inspektor: Add style checker
    Use python-pep8 to point out style non compliances. This initial
    commit implements only the style checker, the style fixer is yet
    to come.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ inspektor/style.py
@@ -0,0 +1,73 @@
+import argparse
+import logging
+import os
+import sys
+
+import pep8
+
+from inspector import PathInspector
+
+log = logging.getLogger("inspektor.style")
+
+class StyleChecker(object):
+    def __init__(self, verbose=True):
+        self.verbose = verbose
+        self.ignored_errors = 'E501,W601'
+        # Be able to analyze all imports inside the project
+        sys.path.insert(0, os.getcwd())
+        self.failed_paths = []
+
+    def set_verbose(self):
+        self.verbose = True
+
+    def check_dir(self, path):
+        """
+        Recursively go on a directory checking files with PEP8.
+
+        :param path: Path to a directory.
+        """
+        def visit(arg, dirname, filenames):
+            for filename in filenames:
+                self.check_file(os.path.join(dirname, filename))
+
+        os.path.walk(path, visit, None)
+        return not self.failed_paths
+
+    def check_file(self, path):
+        """
+        Check one regular file with pylint for py syntax errors.
+
+        :param path: Path to a regular file.
+        :return: False, if pylint found syntax problems, True, if pylint didn't
+                 find problems, or path is not a python module or script.
+        """
+        inspector = PathInspector(path)
+        if not inspector.is_python():
+            return True
+        opt_obj = pep8.StyleGuide().options
+        opt_obj.ignore = tuple(self.ignored_errors.split(','))
+        runner = pep8.Checker(filename=path, options=opt_obj)
+        if runner.check_all() != 0:
+            log.error('PEP8 check fail: %s', path)
+            self.failed_paths.append(path)
+        return runner.check_all() == 0
+
+    def check(self, path):
+        if os.path.isfile(path):
+            return self.check_file(path)
+        elif os.path.isdir(path):
+            return self.check_dir(path)
+
+def run_style(args):
+    path = args.path
+    if not path:
+        path = os.getcwd()
+
+    style_checker = StyleChecker(verbose=args.verbose)
+
+    if style_checker.check(path):
+        log.info("Syntax check PASS")
+        sys.exit(0)
+    else:
+        log.error("Syntax check FAIL")
+        sys.exit(1)
