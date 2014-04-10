05658a3ae561ef42565bb7d28ed04a70367228f6
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-09 21:58:32 -0300

    inspektor: Add license headers in all python files
    Courtesy of the newest feature implemented.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat 2013-2014
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
 from distutils.core import setup
 
 import inspektor.version
@@ -12,4 +26,6 @@ setup(name='inspektor',
                 'inspektor.cli',
                 'inspektor.utils'
                 ],
-      scripts=['scripts/inspekt'])
+      scripts=['scripts/inspekt'],
+      data_files=[('/usr/share/inspektor/data', ['data/LICENSE_SNIPPET_GPLV2'])],
+      )
