73f3ee856f6d1abefdc48ede9b2326e53d08ba7c
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-16 00:28:10 -0300

    documentation: Add GPL header to conf.py
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
 #
 # inspektor documentation build configuration file, created by
 # sphinx-quickstart on Tue Apr 15 02:47:18 2014.
