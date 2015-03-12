fac56a5b03d6c43ec366c45d76ebf3e03fd1e622
Lucas Meneghel Rodrigues <lmr@redhat.com>
2015-03-11 21:54:47 -0300

    inspektor: Bump version to 0.1.15
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- inspektor/version.py
+++ inspektor/version.py
@@ -19,7 +19,7 @@ __all__ = ['MAJOR', 'MINOR', 'RELEASE', 'VERSION']
 
 MAJOR = 0
 MINOR = 1
-RELEASE = 14
+RELEASE = 15
 
 VERSION = "%s.%s.%s" % (MAJOR, MINOR, RELEASE)
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from distutils.core import setup
 
 setup(name='inspektor',
-      version='0.1.14',
+      version='0.1.15',
       description='Inspektor code checker',
       author='Lucas Meneghel Rodrigues',
       author_email='lmr@redhat.com',
